1406f3136f59c2b802d27a2b0df6b5820b5cb6c0
homebase3 <65870950+homebase3@users.noreply.github.com>
2022-12-25 09:55:27 -0600

    Simplify for DBT cloud demo

--- dbt/dbt_entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cp -r /data/. /dbt/data &&
-dbt debug --profiles-dir . &&
-dbt seed --profiles-dir . &&
-dbt deps --profiles-dir . &&
-dbt run --profiles-dir . &&
-dbt test --profiles-dir .  &&
-dbt docs generate --profiles-dir . &&
-dbt docs serve --profiles-dir .
